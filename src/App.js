3358986b24550804ccfef9b1e67722f771883547
Priyanshu_Agrawal <priyanshu100agrawal@gmail.com>
2022-10-09 16:02:50 +0530

    Added the data and some notes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import JSON from './demo.json'
 import { useState } from 'react';
 function App() {
 
-  const [curr,setFilter]=useState("");
+  const [curr,setFilter]=useState("");// taking an empty strinng as the current value.
   
 
 
@@ -11,12 +11,12 @@ function App() {
     <div className="App">
       <div className='container'>
         <input className='inp' onChange={(event)=>setFilter( event.target.value)} placeholder="Search....." />
-
+          {/* on any change to the input tag, re-setting(updating) the current value in useState */}
         {JSON.filter((val)=>{
-          if(curr=="")
+          if(curr=="")    // filtering through the values, every time checking the whole data, not a very efficient way though
           return val;
           else if(val.first_name.toLowerCase().includes(curr.toLowerCase()))
-            return val;
+            return val;       // using .includes, which automatically checks if what you typed has the substring or not 
         })
         .map
         (element => 
