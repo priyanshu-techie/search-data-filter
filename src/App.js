0c4f8c663ad6cbf4a48ea658302d552dd9d88dac
Priyanshu_Agrawal <priyanshu100agrawal@gmail.com>
2022-10-09 15:47:46 +0530

    COMPLETED

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
-
+import JSON from './demo.json'
+import { useState } from 'react';
 function App() {
+
+  const [curr,setFilter]=useState("");
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <input className='inp' onChange={(event)=>setFilter( event.target.value)} placeholder="Search....." />
+
+        {JSON.filter((val)=>{
+          if(curr=="")
+          return val;
+          else if(val.first_name.toLowerCase().includes(curr.toLowerCase()))
+            return val;
+        })
+        .map
+        (element => 
+        <div>
+          <p>{element.first_name}</p>
+          </div>
+          )}
+
+      </div>
     </div>
   );
 }
